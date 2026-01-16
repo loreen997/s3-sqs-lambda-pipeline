cb1cf9a30fa940bbe1a0c59786d97a1612ad8dc1
loreen997 <clorenzofu.inf@upsa.es>
2026-01-16 19:50:35 +0100

    estructura para cloudform

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,6 +1,3 @@
-import json
-
 def lambda_handler(event, context):
-    for record in event["Records"]:
-        print("SQS record body:", record["body"])
-    return {"ok": True}
+    print("Lambda viva")
+    return {"status": "ok"}
